54d78e575974ee5abb313f6dffdfebddb0b931d1
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-11 13:59:37 -0600

    refactor: albums_migration.sql code to include DROP and refactor data types

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    release_date INT(4),
-    sales FLOAT,
-    genre VARCHAR(255),
+    artist VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+    name VARCHAR(50) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(100),
     PRIMARY KEY(id)
 );
-
-DESCRIBE albums;

3fa2951ebe114ef9af004c59bf978ef9196a2378
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-11 16:11:25 -0600

    add: basic-statements.sql file for practicing INSERT

---
+++ basic-statements.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name) VALUES ('Avenged Sevenfold', 'Back Country');
+INSERT INTO albums(artist, name) VALUES ('Avenged Sevenfold', 'Back Country');
+INSERT INTO albums(artist, name) VALUES ('Avenged Sevenfold', 'Back Country');
+INSERT INTO albums(artist, name) VALUES ('Avenged Sevenfold', 'Back Country');
+INSERT INTO albums(artist, name) VALUES ('Avenged Sevenfold', 'Back Country');
+
+INSERT INTO albums(artist, name, release_date)
+VALUES ('Avenged Sevenfold', 'Back Country', 2001),
+       ('Avenged Sevenfold', 'Back Country', 2001),
+       ('Avenged Sevenfold', 'Back Country', 2001),
+       ('Avenged Sevenfold', 'Back Country', 2001),
+       ('Avenged Sevenfold', 'Back Country', 2001);

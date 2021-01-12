27871e508acef87d09e1fb03ea9d5938ca196b8c
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-12 10:44:32 -0600

    refactor: select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,30 +1,30 @@
 USE codeup_test_db;
 
-SELECT 'The name of all albums by Pink Floyd.' AS '';
-SELECT name FROM albums
+SELECT name AS 'The name of all albums by Pink Floyd'
+FROM albums
 WHERE artist = 'Pink Floyd';
 
 
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS '';
-SELECT release_date FROM albums
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released'
+FROM albums
 WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 
-SELECT 'The genre for Nevermind' AS '';
-SELECT genre FROM albums
+SELECT genre AS 'The genre for Nevermind'
+FROM albums
 WHERE name = 'Nevermind';
 
 
-SELECT 'Albums that were released in the 1990s' AS '';
-SELECT name FROM albums
+SELECT name AS 'Albums that were released in the 1990s'
+FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
 
 
-SELECT 'Albums that had less than 20 million certified sales' AS '';
-SELECT name FROM albums
+SELECT name AS 'Albums that had less than 20 million certified sales'
+FROM albums
 WHERE sales < 20;
 
 
-SELECT 'All the albums with a genre of ''Rock''.' AS '';
-SELECT name FROM albums
+SELECT name AS 'All the albums with a genre of ''Rock''.'
+FROM albums
 WHERE genre ='Rock';

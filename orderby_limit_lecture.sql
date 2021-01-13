d5293641c82c63d57efdae1e6c38aa3414b35b6d
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-13 14:05:20 -0600

    notes: update order by limit notes

--- orderby_limit_lecture.sql
+++ orderby_limit_lecture.sql
@@ -1,11 +1,14 @@
 USE employees;
 
-SELECT first_name, last_name
+SELECT first_name, last_name, birth_date
 FROM employees
-ORDER BY last_name DESC, first_name;
+ORDER BY first_name DESC, last_name, birth_date;
 
-SELECT * FROM employees;
+SELECT * FROM employees
+LIMIT 15 OFFSET 5;
 
 SELECT first_name, last_name, birth_date
 FROM employees
 ORDER BY birth_date DESC;
+
+DESCRIBE employees;

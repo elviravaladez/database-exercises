73fe888a451d691d54f1a03651dc23901e80b1b3
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-14 11:33:55 -0600

    refactor: Group By Exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -37,19 +37,25 @@ GROUP BY last_name, first_name;
     # Qiwen
 SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%'
+WHERE last_name LIKE '%q%' #The wild cards % are saying that you the 'q' can have a letter before it or after it
   AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
 
 
+# TODO: Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+  OR last_name LIKE 'E%e' #Another way to check if the last name starts and ends with an e
+GROUP BY last_name
+ORDER BY last_name;
 
-# TODO: Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and
-#  GROUP BY to find the number of employees for each gender with those names.
-#  Your results should be:
+
+# TODO: Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
     # 441 M
     # 268 F
-SELECT COUNT(*) AS total, gender
+SELECT COUNT(gender) AS total, gender
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-GROUP BY gender
-ORDER BY total DESC;
+GROUP BY gender;
